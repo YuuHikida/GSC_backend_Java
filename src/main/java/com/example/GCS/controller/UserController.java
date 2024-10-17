e3d13d08ef51e483c8536c7bce556c1bd70e8e79
hikida <ponponda0103@gmail.com>
2024-10-17 22:54:10 +0900

    In Progress

---
+++ src/main/java/com/example/GCS/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.GCS.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+* 概要:ルート
+* */
+@RestController
+@RequestMapping("/")
+public class UserController {
+    //service
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserServive userServive)
+    {
+        this.userService = userServive;
+    }
+
+    @GetMapping
+    public
+}
