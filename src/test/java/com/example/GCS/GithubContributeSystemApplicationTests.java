221ee1eeb8dc4d03df26587b4bd285aeaa835b0d
hikida <ponponda0103@gmail.com>
2024-10-17 21:55:26 +0900

    first push

---
+++ src/test/java/com/example/GCS/GithubContributeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GCS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubContributeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
