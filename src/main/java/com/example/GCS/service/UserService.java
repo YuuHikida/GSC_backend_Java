192690febe9958b3b4a5858e5c7814cbd71548a4
hikida <ponponda0103@gmail.com>
2024-10-20 21:01:36 +0900

    dayliy

--- src/main/java/com/example/GCS/service/UserService.java
+++ src/main/java/com/example/GCS/service/UserService.java
@@ -3,28 +3,28 @@ package com.example.GCS.service;
 import com.example.GCS.model.UserModel;
 
 import com.example.GCS.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
 
+    @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     /*
-    * 概要   : ユーザ情報を1件取得
+    * 概要   : (gitNameで)ユーザ情報を1件取得
     * 戻り値 : UserModel...
     * */
-    public UserModel GetOneUsersInformation(){
-        //userModel作成
-        // MongoDBAtlas初期化&送る
-        //(nullチェック)
-        //モデルを返す
-
+    public List<UserModel> GetOneUsersInformation(String gitName){
+        return userRepository.findByGitName(gitName);
         //return userRepository.save(user);
-        return new UserModel("1","TANAKA","sample@yahoo.co.jp","21:00");
+        //return new UserModel("1","TANAKA","sample@yahoo.co.jp","21:00");
     }
 
 }
