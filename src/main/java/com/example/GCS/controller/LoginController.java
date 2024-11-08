494c1d0bf87a68ea12145242b17a5caf9528d666
hikida <ponponda0103@gmail.com>
2024-11-08 22:06:44 +0900

    in progress authController

--- src/main/java/com/example/GCS/controller/LoginController.java
+++ src/main/java/com/example/GCS/controller/LoginController.java
@@ -1,36 +1,36 @@
-package com.example.GCS.controller;
-
-import com.example.GCS.dto.UserHomeInfoDTO;
-import com.example.GCS.service.HomeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class LoginController {
-
-    private final HomeService homeService;
-
-    @Autowired
-    public LoginController(HomeService homeService) {
-        this.homeService = homeService;
-    }
-
-    @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
-    @GetMapping("/loginSuccess")
-    public UserHomeInfoDTO loginSuccess(OAuth2AuthenticationToken authentication, Model model) {
-        // ユーザー情報を取得
-        UserHomeInfoDTO userHomeInfoDTO = homeService.getUserInfoFromOAuth2(authentication);
-
-        // デバッグログ
-        System.out.println("User Name: " + userHomeInfoDTO.getUserName());
-        System.out.println("User Email: " + userHomeInfoDTO.getEmail());
-
-        return userHomeInfoDTO;
-    }
-
-}
+//package com.example.GCS.controller;
+//
+//import com.example.GCS.dto.UserHomeInfoDTO;
+//import com.example.GCS.service.HomeService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class LoginController {
+//
+//    private final HomeService homeService;
+//
+//    @Autowired
+//    public LoginController(HomeService homeService) {
+//        this.homeService = homeService;
+//    }
+//
+//    @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+//    @GetMapping("/loginSuccess")
+//    public UserHomeInfoDTO loginSuccess(OAuth2AuthenticationToken authentication, Model model) {
+//        // ユーザー情報を取得
+//        UserHomeInfoDTO userHomeInfoDTO = homeService.getUserInfoFromOAuth2(authentication);
+//
+//        // デバッグログ
+//        System.out.println("User Name: " + userHomeInfoDTO.getUserName());
+//        System.out.println("User Email: " + userHomeInfoDTO.getEmail());
+//
+//        return userHomeInfoDTO;
+//    }
+//
+//}
