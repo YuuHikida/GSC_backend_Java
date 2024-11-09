0682dd6fe0c9ffc7a1129bc8c9b57c38ee73dc42
hikida <ponponda0103@gmail.com>
2024-11-09 23:51:25 +0900

    day

--- src/main/java/com/example/GCS/controller/AuthController.java
+++ src/main/java/com/example/GCS/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.example.GCS.controller;
 
 import com.example.GCS.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,13 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthService authService;
 
+    @Value("${google.clientId}") // Google Client ID を .env または application.properties に保存して読み込みます。
+    private String clientId;
+
     @Autowired
     public  AuthController(AuthService authService)
     {
         this.authService = authService;
     }
 
-    @PostMapping("/verify")
-    public
+
+
+
+
+
 
 }
