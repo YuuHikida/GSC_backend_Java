1758269f47ee52360dfc958c3708a63721ae98fd
hikida <ponponda0103@gmail.com>
2024-11-10 17:39:38 +0900

    in progress authcontroller

--- src/main/java/com/example/GCS/controller/AuthController.java
+++ src/main/java/com/example/GCS/controller/AuthController.java
@@ -1,11 +1,28 @@
 package com.example.GCS.controller;
 
 import com.example.GCS.service.AuthService;
+
+import com.google.api.client.auth.oauth2.TokenRequest;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+
+
+
+import com.nimbusds.openid.connect.sdk.claims.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
 
 /*
  * 概要:クライアントからIDトークンを受け取り、検証する
@@ -15,18 +32,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthService authService;
 
-    @Value("${google.clientId}") // Google Client ID を .env または application.properties に保存して読み込みます。
+    @Value("${GOOGLE_CLIENT_ID}") // Google Client ID を .env または application.properties に保存して読み込み
     private String clientId;
 
     @Autowired
-    public  AuthController(AuthService authService)
-    {
+    public AuthController(AuthService authService) {
         this.authService = authService;
     }
 
 
+    @PostMapping("/authenticate")
+    public UserInfo authenticateUser(@RequestParam TokenRequest tokenRequest) throws GeneralSecurityException, IOException
+    {
+        //dbg用
+        System.out.println("clientId =" + clientId);
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
+                .setAudience(Collections.singletonList(clientId))
+                .build();
 
-
+        // トークン検証コード
+        return new UserInfo();
+    }
 
 
 
