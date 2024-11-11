c89358b5f6d1430eec9560aad6145ef2d558a85e
hikida <ponponda0103@gmail.com>
2024-11-11 21:15:13 +0900

    Separation of authentication process

--- src/main/java/com/example/GCS/controller/AuthController.java
+++ src/main/java/com/example/GCS/controller/AuthController.java
@@ -1,28 +1,19 @@
 package com.example.GCS.controller;
 
+import com.example.GCS.dto.UserHomeInfoDTO;
 import com.example.GCS.service.AuthService;
 
 import com.google.api.client.auth.oauth2.TokenRequest;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.gson.GsonFactory;
 
 
-
-import com.nimbusds.openid.connect.sdk.claims.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.*;
-import java.nio.charset.Charset;
 import java.security.GeneralSecurityException;
-import java.util.Collections;
 
 /*
  * 概要:クライアントからIDトークンを受け取り、検証する
@@ -32,8 +23,7 @@ import java.util.Collections;
 public class AuthController {
     private final AuthService authService;
 
-    @Value("${GOOGLE_CLIENT_ID}") // Google Client ID を .env または application.properties に保存して読み込み
-    private String clientId;
+
 
     @Autowired
     public AuthController(AuthService authService) {
@@ -41,17 +31,17 @@ public class AuthController {
     }
 
 
+    /*
+     * 概要 : フロントから受け取ったトークンを確認してデータを返す
+     */
     @PostMapping("/authenticate")
-    public UserInfo authenticateUser(@RequestParam TokenRequest tokenRequest) throws GeneralSecurityException, IOException
+    public UserHomeInfoDTO authenticateAndUserName_UserEmail(@RequestParam("token") String token) throws GeneralSecurityException, IOException
     {
-        //dbg用
-        System.out.println("clientId =" + clientId);
-        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
-                .setAudience(Collections.singletonList(clientId))
-                .build();
+        UserHomeInfoDTO userInfo = new UserHomeInfoDTO();
+         userInfo = authService.authenticatingTheClientId(token);
 
         // トークン検証コード
-        return new UserInfo();
+        return userInfo;
     }
 
 
