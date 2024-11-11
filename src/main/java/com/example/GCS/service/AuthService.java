05b55425fbf42922970e52e3518c976824302810
hikida <ponponda0103@gmail.com>
2024-11-11 21:15:13 +0900

    Separation of authentication process

--- src/main/java/com/example/GCS/service/AuthService.java
+++ src/main/java/com/example/GCS/service/AuthService.java
@@ -1,4 +1,35 @@
 package com.example.GCS.service;
 
+import com.example.GCS.dto.UserHomeInfoDTO;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+
 public class AuthService {
+
+    @Value("${GOOGLE_CLIENT_ID}") // Google Client ID を .env または application.properties に保存して読み込み
+    private String clientId;
+
+    public UserHomeInfoDTO authenticatingTheClientId(String token)throws GeneralSecurityException, IOException
+    {
+        //dbg用
+        System.out.println("clientId =" + clientId);
+
+        UserHomeInfoDTO userInfo = new UserHomeInfoDTO();
+
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
+                .setAudience(Collections.singletonList(clientId))
+                .build();
+
+        GoogleIdToken idToken = verifier.verify(token);
+
+        return userInfo;
+    }
 }
