198f4234210d7aa15cf430e00738bdf8e4f23228
hikida <ponponda0103@gmail.com>
2024-11-15 21:41:20 +0900

    daily

--- src/main/java/com/example/GCS/controller/AuthController.java
+++ src/main/java/com/example/GCS/controller/AuthController.java
@@ -4,10 +4,7 @@ import com.example.GCS.dto.UserHomeInfoDTO;
 import com.example.GCS.service.AuthService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.*;
 import java.security.GeneralSecurityException;
