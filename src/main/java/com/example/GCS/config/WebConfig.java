bf8389959ea53821aa853b159f3d50f870ed7680
hikida <ponponda0103@gmail.com>
2024-11-16 22:55:53 +0900

    daily

--- src/main/java/com/example/GCS/config/WebConfig.java
+++ src/main/java/com/example/GCS/config/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
+    // cros対策
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
