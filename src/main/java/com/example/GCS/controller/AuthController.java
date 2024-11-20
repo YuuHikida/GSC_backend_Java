6075cbcac9b55ce1f16d6a9532e88c6b5ca1b3eb
hikida <ponponda0103@gmail.com>
2024-11-20 20:26:54 +0900

    change getting  requestBody

--- src/main/java/com/example/GCS/controller/AuthController.java
+++ src/main/java/com/example/GCS/controller/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.*;
 import java.security.GeneralSecurityException;
+import java.util.Map;
 
 /*
  * 概要:クライアントからIDトークンを受け取り、検証する
@@ -25,10 +26,15 @@ public class AuthController {
 
     /*
      * 概要 : フロントから受け取ったトークンを確認してデータを返す
+     * 、token が JSONのボディ に含まれている場合、@RequestParam ではなく @RequestBody を使う必要がある
      */
     @PostMapping("/authenticate")
-    public UserHomeInfoDTO authenticateAndUserName_UserEmail(@RequestParam("token") String token) throws GeneralSecurityException, IOException
+    public UserHomeInfoDTO authenticateAndUserName_UserEmail(@RequestBody Map<String, String> payload) throws GeneralSecurityException, IOException
     {
+        String token = payload.get("token");
+        if (token == null || token.isEmpty()) {
+            throw new IllegalArgumentException("Token is missing or empty");
+        }
         // トークン検証コード
         return authService.authenticatingTheClientId(token);
     }
