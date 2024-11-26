141b0314f28b5d7809b0dcb2a33991b1c68a4e51
hikida <ponponda0103@gmail.com>
2024-11-26 21:12:34 +0900

    プロとして───────TDD ^^

--- src/main/java/com/example/GCS/service/auth/implenebtation/GoogleAuthService.java
+++ src/main/java/com/example/GCS/service/auth/implenebtation/GoogleAuthService.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 
+// SOLID_OCPの原則を適用してinterfaceに
 @Service
 public class GoogleAuthService implements TokenVerifier {
 
