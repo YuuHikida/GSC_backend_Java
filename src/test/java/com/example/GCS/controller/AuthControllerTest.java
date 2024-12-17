a8344cf322d456cc1a50a2495c8d3c5652ba81e4
hikida <ponponda0103@gmail.com>
2024-12-17 21:34:42 +0900

    add testCode explanation

--- src/test/java/com/example/GCS/controller/AuthControllerTest.java
+++ src/test/java/com/example/GCS/controller/AuthControllerTest.java
@@ -23,6 +23,7 @@ public class AuthControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    //MockBeanはサービス層など依存関係をロードするために必要
 //    @MockBean
 //    private ObjectMapper objectMapper;
 //    private TokenVerifier tokenVerifier;
@@ -32,6 +33,17 @@ public class AuthControllerTest {
     void TestGetMethod() throws Exception{
         mockMvc.perform(MockMvcRequestBuilders.get("/auth/testAuthenticate"))
                 .andExpect(status().isOk());    //←HTTPステータスを検証
+        /*
+        * なぜ @WithMockUser を付与すると認証できるのか？
+        1. セキュリティコンテキストの設定
+        @WithMockUser を使用すると、テスト実行時に Spring Security が内部的にセキュリティコンテキストにモックされた認証情報を設定します。
+        * これにより、テスト中のリクエストはあたかも認証済みのユーザーからのものであるかのように扱われます。
+
+        2. フィルターのバイパス
+        通常、Spring Security は認証されていないリクエストに対してリダイレクト（例: 302ステータスコード）を返します。
+        * しかし、@WithMockUser を使用することで、セキュリティフィルターが認証済みと見なすため、リダイレクトが発生せず、
+        * テスト対象のエンドポイントが正常に呼び出されます
+        * */
     }
 //    @Test
 //    void testAuth() throws Exception{
